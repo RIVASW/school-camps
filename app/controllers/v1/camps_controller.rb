da981e69a9b89a3ed986fffb006ea6ddb401374f
Ivan Romaniuk <van.romanyuk@jnsolutions.com.au>
2020-01-30 09:06:06 +1100

    add sceleton react app

---
+++ app/controllers/v1/camps_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class V1::CampsController < ApplicationController
+  def index
+    render(json: {
+      name: 'summer camp',
+      time: 'whole summer from 8am to 7pm'
+    }.to_json)
+  end
+end
