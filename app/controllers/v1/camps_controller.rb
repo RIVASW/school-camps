987d8994a7300152144a6772d05d04d1d65f5301
Ivan Romaniuk <van.romanyuk@jnsolutions.com.au>
2020-02-27 09:11:49 +1100

    add camps/id end point

--- app/controllers/v1/camps_controller.rb
+++ app/controllers/v1/camps_controller.rb
@@ -1,10 +1,59 @@
 # frozen_string_literal: true
 
 class V1::CampsController < ApplicationController
+  CAMPS_LIST = [
+      {
+        id: 1,
+        name: 'PCYC',
+        location: 'North Sydney',
+        price: 60,
+        contacts: "0495876398",
+        image: 'http:/...jpg',
+        description: 'PCYC sport camp in North Sydney and other areas of Sydney'
+      },
+      {
+        id: 2,
+        name: 'Big Blue Camp',
+        location: 'North Sydney',
+        price: 120,
+        contacts: "0495876584",
+        image: 'http:/...jpg',
+        description: 'Big Blue camp with different activities including tennis, swimming, soccer'
+      },
+      {
+        id: 3,
+        name: 'Tennis camp',
+        location: 'Cammerey',
+        price: 85,
+        contacts: "0495556584",
+        image: 'http:/...jpg',
+        description: 'Cammerey tennis camp for kids'
+      },
+      {
+        id: 4,
+        name: 'Leasure center camp',
+        location: 'North Sydney',
+        price: 70,
+        contacts: "0495123584",
+        image: 'http:/...jpg',
+        description: 'Holidays activities in North Sydney leasure center'
+      },
+      {
+        id: 5,
+        name: 'Bricks for kids',
+        location: 'North Sydney',
+        price: 90,
+        contacts: "0495858784",
+        image: 'http:/...jpg',
+        description: 'Bricks for kids Lego activities for kids during school holidays'
+      }
+    ]
   def index
-    render(json: [{
-      name: 'summer camp',
-      time: 'whole summer from 8am to 7pm'
-    }].to_json)
+    render(json: CAMPS_LIST.to_json)
+  end
+
+  def show
+    the_camp = CAMPS_LIST.detect { |camp| camp[:id] == params[:id].to_i }
+    render(json: the_camp.to_json)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :v1, defaults: { format: 'json' } do
     get 'camps', to: 'camps#index'
+    get 'camps/:id', to: 'camps#show'
   end
 
   get '*page', to: 'static#index', constraints: ->(req) { !req.xhr? && req.format.html? }
