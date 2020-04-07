4aea7548d6cfe807a5b25805791fe9650479290e
Natalia Romanyuk <feliz@MacBook-Pro.modem>
2020-04-07 15:40:16 +1000

    fixed delete button

--- app/javascript/components/CampListItem.jsx
+++ app/javascript/components/CampListItem.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import photo1 from '../images/02.jpg';
+import { deleteCamp } from '../actions/deleteCamp';
 
-const CampListItem = (props) => {
-    const {id, location, name, isAdmin} = props;
+class CampListItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
+    onDelete = () => {
+        const { dispatch } = this.props;
+        dispatch(deleteCamp(this.props.id));
+    }
+
+    render(){
     return (
         <div className="col-md-4">
             <div className="card mb-4 box-shadow">
@@ -13,20 +22,25 @@ const CampListItem = (props) => {
             <img src={photo1} className="card-img-top" alt="Thumbnail [100%x225]" />
 
                 <div className="card-body">
-                    <Link to={`/camps/${id}`}>
-                        <p className="card-text">{name}</p>
+                    <Link to={`/camps/${this.props.id}`}>
+                        <p className="card-text">{this.props.name}</p>
                     </Link>
-                        {/* <p className="card-text">{location}</p> */}
-                </div>
+                        <p>{this.props.location}</p>
 
-                {isAdmin && 
-                    <Link to={`/edit/${id}`}>
-                        <p>EDIT</p>
-                    </Link>
-                }
+                    <div className="btn-group">
+                        {this.props.isAdmin && 
+                            <Link to={`/edit/${this.props.id}`}>
+                                <button type="button" className="btn btn-sm btn-outline-secondary">edit</button>
+                            </Link>
+                        }
+                        {this.props.isAdmin && 
+                        <button type="button" className="btn btn-sm btn-outline-secondary" onClick={this.onDelete}>delete</button>
+                        }
+                    </div>
+                </div>
             </div> 
         </div>
-    )
+    )}
 };
 
 const mapStateToProps = ((state) => {
@@ -36,4 +50,3 @@ const mapStateToProps = ((state) => {
 });
   
 export default connect (mapStateToProps)(CampListItem);
-
